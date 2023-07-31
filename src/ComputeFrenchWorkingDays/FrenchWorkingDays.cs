3edf1fdddac82a717e0bdfd3ec1bfe2ba36c3f44
Ksi <labani.said@outlook.com>
2023-07-31 22:43:31 +0200

    Add project files.

---
+++ src/ComputeFrenchWorkingDays/FrenchWorkingDays.cs
@@ -0,0 +1,124 @@
+﻿namespace ComputeFrenchWorkingDays
+{
+    public static class FrenchWorkingDays
+    {
+        private static readonly DateOnly[] FixedHolidayDates = new DateOnly[]
+        {
+            new DateOnly(2000, 1, 01),
+            new DateOnly(2000, 5, 01),
+            new DateOnly(2000, 5, 08),
+            new DateOnly(2000, 7, 14),
+            new DateOnly(2000, 8, 15),
+            new DateOnly(2000, 11, 1),
+            new DateOnly(2000, 11, 11),
+            new DateOnly(2000, 12, 25),
+        };
+
+        public static DateTime AddFrenchWorkingDays(this DateTime dateTime, int workingDays)
+        {
+            var dateOnly = DateOnly.FromDateTime(dateTime);
+            dateOnly = AddFrenchWorkingDays(dateOnly, workingDays);
+
+            var newDateTime = dateOnly.ToDateTime(TimeOnly.FromDateTime(dateTime));
+
+            return newDateTime;
+        }
+
+        public static DateOnly AddFrenchWorkingDays(this DateOnly dateOnly, int workingDays)
+        {
+            if (workingDays == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(workingDays), $"Working days must be a nonzero number.");
+            }
+
+            if (dateOnly.Year < 1583)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dateOnly), $"Year must be greater than or equal to 1583.");
+            }
+
+            var workingDaysOffSet = FindNextWorkingDateOffset(dateOnly, workingDays, Math.Sign(workingDays), new Dictionary<int, DateOnly[]>());
+
+            return dateOnly.AddDays(workingDaysOffSet);
+        }
+
+        private static int FindNextWorkingDateOffset(DateOnly date, int days, int step, Dictionary<int, DateOnly[]> yearsHolidays)
+        {
+            if (days == 0)
+            {
+                return 0;
+            }
+
+            date = date.AddDays(step);
+
+            if (!yearsHolidays.ContainsKey(date.Year))
+            {
+                var holidays = GetYearHolidays(date.Year);
+                yearsHolidays.Add(date.Year, holidays);
+            }
+
+            days = IsWorkingDay(date, yearsHolidays[date.Year]) ? days - step : days;
+
+            return FindNextWorkingDateOffset(date, days, step, yearsHolidays) + step;
+        }
+
+        private static bool IsWorkingDay(DateOnly date, DateOnly[] yearHolidays)
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return false;
+            }
+
+            if (yearHolidays.Contains(date))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static (DateOnly EasternMondayDate, DateOnly AscentDate, DateOnly PentecostDate) ComputeVariableHolidayDates(int year)
+        {
+            // OUDIN's Algorithm
+            // http://frederic.leon77.free.fr/formations/2012_13/100outils/jour2/oudin.pdf
+            var g = year % 19;
+            var c = year / 100;
+            var c_4 = c / 4;
+            var e = ((8 * c) + 13) / 25;
+            var h = ((19 * g) + c - c_4 - e + 15) % 30;
+            var k = h / 28;
+            var p = 29 / (h + 1);
+            var q = (21 - g) / 28;
+            var i = (((k * p * q) - 1) * k) + h;
+            var b = year + (year / 4);
+            var j1 = b + i + 2 + c_4 - c;
+            var j2 = j1 % 7;
+            var easternDay = 28 + i - j2;
+            var easternMonth = 3;
+            if (easternDay >= 31)
+            {
+                easternDay %= 31;
+                easternMonth = 4;
+            }
+
+            var easternMonday = easternDay + 1;
+            var easternMondayDate = new DateOnly(year, easternMonth, easternMonday);
+
+            return (easternMondayDate, easternMondayDate.AddDays(38), easternMondayDate.AddDays(49));
+        }
+
+        private static DateOnly[] GetYearHolidays(int year)
+        {
+            var fixedHolidays = FixedHolidayDates.Select(d => new DateOnly(year, d.Month, d.Day));
+            var variableHolidays = ComputeVariableHolidayDates(year);
+
+            return fixedHolidays.Concat(new DateOnly[]
+            {
+                variableHolidays.EasternMondayDate,
+                variableHolidays.AscentDate,
+                variableHolidays.PentecostDate,
+            })
+            .OrderBy(d => d.Month)
+            .ToArray();
+        }
+    }
+}
---
+++ tests/ComputeFrenchWorkingDays/FrenchWorkingDaysTest.cs
@@ -0,0 +1,83 @@
+﻿namespace ComputeFrenchWorkingDays.Tests
+{
+    public class FrenchWorkingDaysTest
+    {
+        [Theory]
+        [InlineData("2023-07-31", 1, "2023-08-01")]
+        [InlineData("2023-07-31", 2, "2023-08-02")]
+        [InlineData("2023-12-23", 3, "2023-12-28")]
+        [InlineData("2023-12-24", 1, "2023-12-26")]
+        [InlineData("2023-07-14", 1, "2023-07-17")]
+        [InlineData("2023-07-14", -2, "2023-07-12")]
+        public void AddFrenchWorkingDays_DateOnly_ShouldReturnCorrectDate(string inputDate, int workingDaysToAdd, string expectedDate)
+        {
+            // Arrange
+            var inputDateTime = DateOnly.Parse(inputDate);
+            var expectedDateTime = DateOnly.Parse(expectedDate);
+
+            // Act
+            var resultDateTime = inputDateTime.AddFrenchWorkingDays(workingDaysToAdd);
+
+            // Assert
+            resultDateTime.Should().Be(expectedDateTime);
+        }
+
+        [Theory]
+        [InlineData("2023-07-31", 0, "Working days must be a nonzero number. (Parameter 'workingDays')")]
+        [InlineData("1200-07-31", 2, "Year must be greater than or equal to 1583. (Parameter 'dateOnly')")]
+        public void AddFrenchWorkingDays_DateOnly_OutOfRangeException(string inputDate, int workingDaysToAdd, string expectedExceptionMessage)
+        {
+            // Arrange
+            var inputDateTime = DateTime.Parse(inputDate);
+
+            // Act
+            var act = () => inputDateTime.AddFrenchWorkingDays(workingDaysToAdd);
+
+            // Assert
+            act
+                .Should()
+                .Throw<ArgumentOutOfRangeException>()
+                .WithMessage(expectedExceptionMessage);
+        }
+
+        [Theory]
+        [InlineData("2023-07-31 12:34:56", 1, "2023-08-01 12:34:56")]
+        [InlineData("2023-07-31 18:45:00", 2, "2023-08-02 18:45:00")]
+        [InlineData("2023-12-23 09:00:00", 3, "2023-12-28 09:00:00")]
+        [InlineData("2023-12-24 14:30:00", 1, "2023-12-26 14:30:00")]
+        [InlineData("2023-07-14 16:40:00", 1, "2023-07-17 16:40:00")]
+        [InlineData("2023-07-14 22:50:00", -2, "2023-07-12 22:50:00")]
+        public void AddFrenchWorkingDays_DateTime_ShouldReturnCorrectDate(string inputDate, int workingDaysToAdd, string expectedDate)
+        {
+            // Arrange
+            var inputDateTime = DateTime.Parse(inputDate);
+            var expectedDateTime = DateTime.Parse(expectedDate);
+
+            // Act
+            var resultDateTime = inputDateTime.AddFrenchWorkingDays(workingDaysToAdd);
+
+            // Assert
+            resultDateTime.Should().Be(expectedDateTime);
+        }
+
+        [Theory]
+        [InlineData("2023-07-31 18:45:00", 0, "Working days must be a nonzero number. (Parameter 'workingDays')")]
+        [InlineData("1200-07-31 02:15:00", 2, "Year must be greater than or equal to 1583. (Parameter 'dateOnly')")]
+        public void AddFrenchWorkingDays_DateTime_OutOfRangeException(string inputDate, int workingDaysToAdd, string expectedExceptionMessage)
+        {
+            // Arrange
+            var inputDateTime = DateTime.Parse(inputDate);
+
+            // Act
+            var act = () => inputDateTime.AddFrenchWorkingDays(workingDaysToAdd);
+
+            // Assert
+            act
+                .Should()
+                .Throw<ArgumentOutOfRangeException>()
+                .WithMessage(expectedExceptionMessage);
+        }
+
+
+    }
+}
